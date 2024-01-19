3d03b02ce813c02ed5f99bfcc79bf03432445a22
mahir mehta <mahirmehta77@gmail.com>
2024-01-19 23:44:43 +0530

    Create Support.js

---
+++ frontend-marketmind/src/components/pages/Support.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Support.css';
+import Footer from '../Footer';
+
+function Support() {
+  return (
+    <div>
+        <h1 className='support'>Support</h1>;
+        <Footer />  
+    </div>
+  )
+}
+
+export default Support
