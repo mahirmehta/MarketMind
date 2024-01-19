db49986566f2c8c4b0038ab480408900ada84e90
mahir mehta <mahirmehta77@gmail.com>
2024-01-19 23:45:07 +0530

    Create General.js

---
+++ frontend-marketmind/src/components/pages/General.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './General.css';
+import Footer from '../Footer';
+
+function General() {
+  return (
+    <div>
+        <h1 className='general'>General</h1>;
+        <Footer />  
+    </div>
+  )
+}
+
+export default General
