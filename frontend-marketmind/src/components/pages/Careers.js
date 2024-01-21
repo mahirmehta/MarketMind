93503808e36431053cf7076f6cbee61bc7161443
mahir mehta <mahirmehta77@gmail.com>
2024-01-21 16:23:37 +0530

    Create Careers.js

---
+++ frontend-marketmind/src/components/pages/Careers.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Careers.css';
+import Footer from '../Footer';
+
+function Careers() {
+  return (
+    <div>
+        <h1 className='careers'>Careers</h1>;
+        <Footer />  
+    </div>
+  )
+}
+
+export default Careers
