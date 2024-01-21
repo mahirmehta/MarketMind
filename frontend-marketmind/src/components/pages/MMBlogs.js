cb4b3daa83aaaddf409dcd4f5f1b7585dff6f381
mahir mehta <mahirmehta77@gmail.com>
2024-01-21 16:23:48 +0530

    Create MMBlogs.js

---
+++ frontend-marketmind/src/components/pages/MMBlogs.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './MMBlogs.css';
+import Footer from '../Footer';
+
+function MMBlogs() {
+  return (
+    <div>
+        <h1 className='mm-blogs'>MarketMind Blogs</h1>;
+        <Footer />  
+    </div>
+  )
+}
+
+export default MMBlogs
