7e128bfb7efceb92f65ced15c1cabca35714e660
mahir mehta <mahirmehta77@gmail.com>
2024-03-04 17:35:39 +0530

    Update Home.js

--- frontend-marketmind/src/components/pages/Home.js
+++ frontend-marketmind/src/components/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../../App.css';
 import HeroSection from '../HeroSection';
+import Table from '../Table';
 import Cards from '../Cards';
 import Footer from '../Footer';
 
@@ -8,6 +9,7 @@ function Home() {
   return (
     <>
       <HeroSection />
+      <Table />
       <Cards />
       <Footer />
     </>
