707dc3db4f9e4f9ade7cd458a306b081c1fb5dad
mahir mehta <mahirmehta77@gmail.com>
2024-03-04 22:59:17 +0530

    Update App.js

--- frontend-marketmind/src/App.js
+++ frontend-marketmind/src/App.js
@@ -3,7 +3,6 @@ import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home';
-import Analysis from './components/pages/Analysis';
 import Dashboard from './components/pages/Dashboard';
 import SignUp from './components/pages/SignUp';
 import Team from './components/pages/Team';
@@ -23,7 +22,6 @@ function App() {
         <Routes>
           <Route exact path='/' element={<Home />} />
           <Route exact path='/Dashboard' element={<Dashboard />} />
-          <Route exact path='/Analysis' element={<Analysis />} />
           <Route exact path='/sign-up' element={<SignUp />} />
           <Route exact path='/team' element={<Team />} />
           <Route exact path='/about-us-&-contact-us' element={<AboutUs />} />
