f8fdd71cdd1acce29cf108328a02338ad0b6308a
mahir mehta <mahirmehta77@gmail.com>
2024-03-06 00:42:58 +0530

    backend work

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import serializers
+from rest_framework import viewsets
+from .serializers import CompanySerializer,UserSerializer
+from .models import Company
+from django.contrib.auth.models import User
 
-# Create your views here.
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class CompanyViewSet(viewsets.ModelViewSet):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+    authentication_classes = (TokenAuthentication,)
