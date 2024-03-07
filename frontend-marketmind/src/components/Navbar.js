1034f49dedace008423dbc788a909e2eeb4a8cb2
mahir mehta <mahirmehta77@gmail.com>
2024-03-07 15:22:15 +0530

    Update Navbar.js

--- frontend-marketmind/src/components/Navbar.js
+++ frontend-marketmind/src/components/Navbar.js
@@ -3,34 +3,39 @@ import { Button } from './Button';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
-function Navbar() {
+function Navbar({ isLoggedIn, handleLogout }) {
     const [click, setClick] = useState(false);
     const [button, setButton] = useState(true);
-  
+
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
-  
+
     const showButton = () => {
-      if (window.innerWidth <= 960) {
-        setButton(false);
-      } else {
-        setButton(true);
-      }
+        if (window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
     };
-  
+
     useEffect(() => {
-      showButton();
+        showButton();
     }, []);
-  
-    window.addEventListener('resize', showButton);
+
+    useEffect(() => {
+        // Check if user is logged in and update the button accordingly
+        if (isLoggedIn) {
+            setButton(false); // Hide Sign Up button if user is logged in
+        }
+    }, [isLoggedIn]);
 
     return (
         <>
             <nav className="navbar">
                 <div className="navbar-container">
                     <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-                        MarketMind 
-                    </Link>    
+                        MarketMind
+                    </Link>
                     <div className='menu-icon' onClick={handleClick}>
                         <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
                     </div>
@@ -41,34 +46,40 @@ function Navbar() {
                             </Link>
                         </li>
                         <li className='nav-item'>
-                            <Link 
-                                to='/dashboard' 
-                                className='nav-links' 
+                            <Link
+                                to='/dashboard'
+                                className='nav-links'
                                 onClick={closeMobileMenu}
                             >
                                 Dashboard
                             </Link>
                         </li>
                         <li className='nav-item'>
-                            <Link 
-                                to='/about-us-&-contact-us' 
-                                className='nav-links' 
+                            <Link
+                                to='/about-us-&-contact-us'
+                                className='nav-links'
                                 onClick={closeMobileMenu}
                             >
                                 About Us
                             </Link>
                         </li>
-                        <li className='nav-item'>
-                            <Link 
-                                to='/sign-up' 
-                                className='nav-links-mobile' 
-                                onClick={closeMobileMenu}
-                            >
-                                Sign up
-                            </Link>
-                        </li>
-                    </ul>      
-                    {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}    
+                        {isLoggedIn ? (
+                            <li className='nav-item'>
+                                <Button buttonStyle="btn--logout" onClick={handleLogout}>Log Out</Button>
+                            </li>
+                        ) : (
+                            <li className='nav-item'>
+                                <Link
+                                    to='/sign-up'
+                                    className='nav-links-mobile'
+                                    onClick={closeMobileMenu}
+                                >
+                                    Sign up
+                                </Link>
+                            </li>
+                        )}
+                    </ul>
+                    {button && !isLoggedIn && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
                 </div>
             </nav>
         </>
