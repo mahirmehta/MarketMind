c00e95fc5b7a085e0ac2e54c13fa135ac4a9cf2d
mahir mehta <mahirmehta77@gmail.com>
2024-03-08 18:19:32 +0530

    Update APIService.js

--- frontend-marketmind/src/components/APIService.js
+++ frontend-marketmind/src/components/APIService.js
@@ -1,22 +1,24 @@
 export default class APISerive {
 
-    static LoginUser(body){
-        return fetch(`http://127.0.0.1:8000/auth/`,{
-            method:'POST',
+    static async LoginUser(body){
+        const resp = await fetch(`http://127.0.0.1:8000/auth/`, {
+            method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body:JSON.stringify(body)
-        }).then(resp => resp.json())
+            body: JSON.stringify(body)
+        })
+        return await resp.json()
     }
 
-    static RegisterUser(body){
-        return fetch(`http://127.0.0.1:8000/api/users/`, {
-            method:'POST',
+    static async RegisterUser(body){
+        const resp = await fetch(`http://127.0.0.1:8000/api/users/`, {
+            method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body:JSON.stringify(body)
-        }).then(resp => resp.json())
+            body: JSON.stringify(body)
+        })
+        return await resp.json()
     }    
 }
