7c37ab4e74da971435f12c11dd6946d195aa7355
mahir mehta <mahirmehta77@gmail.com>
2024-03-08 12:02:45 +0530

    Update index.js

--- frontend-marketmind/src/index.js
+++ frontend-marketmind/src/index.js
@@ -1,5 +1,15 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = document.getElementById('root');
+
+// Use createRoot instead of ReactDOM.render
+const rootElement = ReactDOM.createRoot(root);
+rootElement.render(<App />);
